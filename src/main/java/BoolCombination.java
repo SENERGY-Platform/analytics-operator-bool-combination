ad9f02a2aaf71cc8314f8c41f42666aa015f7263
OZ <60325609+ottmastero@users.noreply.github.com>
2021-02-12 16:27:56 +0100

    implement bool-combination

---
+++ src/main/java/BoolCombination.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2021 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.infai.ses.senergy.exceptions.NoValueException;
+import org.infai.ses.senergy.operators.BaseOperator;
+import org.infai.ses.senergy.operators.Message;
+
+public class BoolCombination extends BaseOperator {
+
+    final String windowOpen = "Door/Window Open";
+    final String windowClosed = "Door/Window Closed";
+
+
+    @Override
+    public Message configMessage(Message message){
+        message.addFlexInput("windowStatus");
+        message.addFlexInput("tiltStatus");
+        return message;
+    }
+
+    @Override
+    public void run(Message message) {
+        String windowStatus = "";
+        Boolean tiltStatus = false;
+        String status = null;
+        try {
+            windowStatus = message.getFlexInput("windowStatus").getString();
+            tiltStatus = Boolean.parseBoolean(message.getFlexInput("tiltStatus").getString());
+        } catch (NoValueException e) {
+            e.printStackTrace();
+        }
+        if (windowStatus.equals(windowOpen) && tiltStatus){
+            status="tilted";
+        } else if (windowStatus.equals(windowOpen) && !tiltStatus){
+            status="opened";
+        } else{
+            status="closed";
+        }
+        message.output("status", status);
+
+    }
+}
---
+++ src/test/java/BoolCombintationTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.infai.ses.senergy.models.DeviceMessageModel;
+import org.infai.ses.senergy.models.MessageModel;
+import org.infai.ses.senergy.operators.Config;
+import org.infai.ses.senergy.operators.Helper;
+import org.infai.ses.senergy.operators.Message;
+import org.infai.ses.senergy.testing.utils.JSONHelper;
+import org.infai.ses.senergy.utils.ConfigProvider;
+import org.json.simple.JSONArray;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BoolCombintationTest {
+
+    private final JSONArray messages = new JSONHelper().parseFile("messages.json");
+    private final String configString = new JSONHelper().parseFile("config.json").toString();
+
+    @Test
+    public void testRun(){
+        Config config = new Config(configString);
+        String topicName = config.getInputTopicsConfigs().get(0).getName();
+        ConfigProvider.setConfig(config);
+        Message message = new Message();
+        MessageModel model =  new MessageModel();
+        BoolCombination boolCombination = new BoolCombination();
+        boolCombination.configMessage(message);
+        int index = 0;
+        String expected []  = new String []{"closed","tilted","opened"};
+        for(Object msg : messages){
+            DeviceMessageModel deviceMessageModel = JSONHelper.getObjectFromJSONString(msg.toString(), DeviceMessageModel.class);
+            assert deviceMessageModel != null;
+            model.putMessage(topicName, Helper.deviceToInputMessageModel(deviceMessageModel, topicName));
+            message.setMessage(model);
+            boolCombination.run(message);
+            Assert.assertEquals(expected[index++], message.getMessage().getOutputMessage().getAnalytics().get("status"));
+        }
+    }
+}
